eab3fe178b33b6f6fc956c72af26e1e7f3903c44
SwordShieldMouse <alke8882000@gmail.com>
2019-02-04 00:24:16 -0700

    added convolutional gru unit

--- architectures.py
+++ architectures.py
@@ -13,12 +13,13 @@ def get_padding(kernel):
     return (kernel - 1) // 2
 
 class State_Rep(nn.Module):
-    # common class for converting observation image into something meaningful
-    # somehow put in a relational neural net
-
+    # TODO: how do we keep track of motion? do we need a replay buffer? an rnn?
+        # important for: dodging projectiles, assessing movement of enemies for aiming
     def __init__(self, c, h, w, attn_heads = 3):
         super(State_Rep, self).__init__()
         self.c = c
+        self.h = h
+        self.w = w
         self.attn_heads = 3
         self.final_channel_size = 5
 
@@ -40,22 +41,60 @@ class State_Rep(nn.Module):
 
         self.relational_nn = Relational_NN(self.final_channel_size, attn_heads)
 
+        self.conv_grus = nn.ModuleList([Conv_GRU(c, h, w), Conv_GRU(c, h, w)])
+        self.hs = torch.rand([2, c, h, w], requires_grad = True).to(device)
+
     def forward(self, x):
         # give a batch size since nn.conv2d requires it
         x = torch.unsqueeze(x, 0)
         # Gym gives us inputs in the form (h, w, c), so convert to (c, h, w)
-        #print(x.shape)
+
         x = x.permute(0, 3, 1, 2)
-        x = self.conv_layers(x)
+        conv_x = self.conv_layers(x)
+
+        # run the relational network
         relations = self.relational_nn(x)
-        #print(relations.shape)
-        return torch.cat([x.view(1, -1), relations], dim = -1)
+
+        # run the GRU
+        for ix in range(2):
+            conv_gru_out, self.hs[ix] = conv_grus[ix](x, self.hs[ix])
+
+        conv_gru_out = conv_gru_out.view(1, -1)
+
+        return torch.cat([conv_x.view(1, -1), relations, conv_gru_out], dim = -1)
 
     def get_final_img_shape(self):
         return self.final_channel_size, self.final_h, self.final_w
 
     def get_out_len(self):
-        return self.final_channel_size * self.final_h * self.final_w + self.attn_heads * self.final_channel_size
+        return self.final_channel_size * self.final_h * self.final_w + self.attn_heads * self.final_channel_size + self.c * self.h * self.w
+
+class Conv_GRU(nn.Module):
+    # a convolutional GRU that takes an image, performs convolutions, then passes it through a GRU
+    def __init__(self, c, h, w):
+        super(Conv_GRU, self).__init__()
+        self.c, self.h, self.w = c, h, w
+
+        self.r_convs = nn.ModuleList()
+        self.z_convs = nn.ModuleList()
+        self.n_convs = nn.ModuleList()
+
+        for _ in range(1):
+            self.r_convs.append(nn.Conv2d(c, 5, kernel_size = 3, padding = get_padding(3)))
+            self.z_convs.append(nn.Conv2d(c, 5, kernel_size = 3, padding = get_padding(3)))
+            self.n_convs.append(nn.Conv2d(c, 5, kernel_size = 3, padding = get_padding(3)))
+
+        #self.h = torch.rand([c, h, w], requires_grad = True).to(device)
+
+    def forward(self, x, h):
+        r = F.sigmoid(self.r_convs[0](x) + self.r_convs[1](h))
+        z = F.sigmoid(self.z_convs[0](x) + self.z_convs[1](h))
+        n = F.tanh(self.n_convs[0](x) + r * self.n_convs[1](h))
+        h = (1 - z) * n + z * h
+
+        return h, h
+
+
 
 class Relational_NN(nn.Module):
     # models relations between objects pairs
@@ -130,6 +169,11 @@ class Value_Fn(nn.Module):
 class Policy(nn.Module):
     # should policy and value function share representation of the image?
     # maybe yes since this reduces training time and things that are relevant for policy should also be relevant for value function?
+    # TODO: agent seems stuck in one position after first episode
+        # seems like it is adhering to unoptimal strategy of waiting for a column to shoot up
+        # but does not discover strategy of hiding behind blocks
+        # mb need to add some signal to explore if reward stays the same or does not increase?
+            # intrinsic reward is self-improvement? how is this different from maximizing return?
     def __init__(self, action_dim, c, h, w, attn_heads = 3):
         super(Policy, self).__init__()
         self.h = h
@@ -151,3 +195,6 @@ class Policy(nn.Module):
         x = self.state_rep(x)
         x = self.linears(x)
         return x
+
+# TODO: try a random search to compare
+# TODO: also try deterministic policy gradient
